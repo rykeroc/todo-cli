d5dfd48299fd936327d5381d89efccecca2101d4
rykeroc <ryker.cooke@gmail.com>
2025-04-25 15:49:19 -0400

    Implement remove sub command

---
+++ cmd/remove.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/rykeroc/todo-cli/internal/data"
+	"github.com/rykeroc/todo-cli/internal/modules/todo"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"os"
+	"strconv"
+)
+
+// removeCmd represents the remove command
+var removeCmd = &cobra.Command{
+	Use:   "remove",
+	Short: "Delete a todo item.",
+	Long:  "Delete an existing todo item by specifying it's ID.",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		idToDelete, err := strconv.ParseInt(args[0], 10, 64)
+		if err != nil {
+			fmt.Println("Unable to remove todo item.")
+			fmt.Printf("'%s' is not a valid ID.\n", args[0])
+			return
+		}
+
+		dataSourceName := os.Getenv("DB_DATASOURCE_NAME")
+		config := data.NewSqliteDatabaseConfig(dataSourceName)
+		db := data.ConnectSqlDatabase(config)
+		domain := todo.NewDomain()
+		repository := todo.NewSqliteRepository(db)
+		useCase := todo.NewUseCase(domain, repository)
+
+		deletedItemId, err := useCase.Remove(idToDelete)
+		if err != nil {
+			log.Errorf("removeCmd: %v", err)
+			fmt.Println("An error occurred while removing the todo item")
+			return
+		}
+		if deletedItemId == -1 {
+			fmt.Printf("No todo item exists with ID %d", idToDelete)
+			return
+		}
+		fmt.Println("Removed item")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(removeCmd)
+}
--- internal/modules/todo/todo_usecase.go
+++ internal/modules/todo/todo_usecase.go
@@ -2,23 +2,40 @@ package todo
 
 import "fmt"
 
+// UseCase godoc
+//
+// An interface that defines the behaviour for a todo item use case struct.
 type UseCase interface {
 	Create(string) error
 	List() error
+	Remove(int64) (int64, error)
 }
 
+// defaultUseCase godoc
+//
+// A structure which takes a todo domain and repository.
+//
+// Adheres to the todo UseCase interface.
 type defaultUseCase struct {
 	domain     Domain
 	repository Repository
 }
 
-func NewUseCase(repository Repository) UseCase {
+// NewUseCase godoc
+//
+// Creates a new UseCase with the passed in Domain and Repository instances.
+func NewUseCase(domain Domain, repository Repository) UseCase {
 	return &defaultUseCase{
-		domain:     NewDomain(),
+		domain:     domain,
 		repository: repository,
 	}
 }
 
+// Create godoc
+//
+// Construct a new todo item using the passed in name and persist it locally.
+//
+// Returns error on error, nil otherwise.
 func (uc *defaultUseCase) Create(name string) error {
 	item, err := uc.domain.CreateItem(name)
 	if err != nil {
@@ -32,16 +49,49 @@ func (uc *defaultUseCase) Create(name string) error {
 	return nil
 }
 
+// List godoc
+//
+// Get all persisted todo items and print them in a tabular list.
+//
+// Returns error on error, nil otherwise.
 func (uc *defaultUseCase) List() error {
 	items, err := uc.repository.FindAllItems()
 	if err != nil {
 		return fmt.Errorf("defaultUseCase.List: %v", err)
 	}
-	tabularList, err := uc.domain.GetItemList(items)
+	tabularList, err := uc.domain.GetTabularItemList(items)
 	if err != nil {
 		return fmt.Errorf("defaultUseCase.List: %v", err)
 	}
 
 	fmt.Println(tabularList)
 	return nil
+}
+
+// Remove godoc
+//
+// Remove an item by its ID.
+//
+// Returns -1 and nil when the item does not exist.
+//
+// Returns -1 and error on error.
+//
+// Returns the deleted item's ID when the item is deleted successfully.
+func (uc *defaultUseCase) Remove(itemId int64) (int64, error) {
+	// Invalid item ID
+	if itemId == 0 {
+		return -1, nil
+	}
+
+	// Delete the item by its ID
+	affectedRows, err := uc.repository.DeleteItemById(itemId)
+	if err != nil {
+		return -1, fmt.Errorf("defaultUseCase.Remove: Failed to delete item by ID: %v", err)
+	}
+	// Item not deleted as it does not exist
+	if affectedRows == 0 {
+		return -1, nil
+	}
+
+	return itemId, nil
 }
